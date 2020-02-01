1091102d9c6676f2c13f9e3f7863a175df6c5ec9
SapirLevy95 <sapir.lev1515@gmail.com>
2020-02-01 12:37:38 +0200

    First commit

---
+++ app/src/main/java/com/example/spliteven/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.spliteven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mloginBtn;
+    Button mregisterBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mloginBtn = findViewById(R.id.login);
+        mregisterBtn = findViewById(R.id.register);
+
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToLoginScreen();
+            }
+        });
+
+        mregisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    private void moveToLoginScreen() {
+
+        Intent intent = new Intent(getApplicationContext(),login_page.class);
+        startActivity(intent);
+
+    }
+
+
+}
+
