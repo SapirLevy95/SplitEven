c7b8137c3d0d73f0d01d581d373975c2a8edac8a
SapirLevy95 <sapir.lev1515@gmail.com>
2020-03-07 19:19:11 +0200

    add approve list

---
+++ app/src/main/java/com/example/split_even/ApproveItemsAdapter.java
@@ -0,0 +1,42 @@
+package com.example.split_even;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ApproveItemsAdapter extends ArrayAdapter<String> {
+
+    private final approveNewItem context;
+    private final ArrayList<String> item_names;
+
+    public ApproveItemsAdapter(approveNewItem context) {
+        super(context, R.layout.shoping_cart_item, context.approveItems);
+        this.context=context;
+        this.item_names=context.approveItems;
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.appoved_item_adapter, null,true);
+
+        TextView titleText = rowView.findViewById(R.id.title);
+        titleText.setText(this.item_names.get(position));
+        Button removeButton= rowView.findViewById(R.id.remove_button);
+
+        removeButton.setOnClickListener(new PositionalOnClickListener(position) {
+            @Override
+            public void onClick(View removeView) {
+                context.remove_item(context.approveItems.get(this.position));
+            }
+        });
+        return rowView;
+
+    };
+}
+
+;
--- app/src/main/java/com/example/split_even/Main_menu.java
+++ app/src/main/java/com/example/split_even/Main_menu.java
@@ -1,25 +1,18 @@
 package com.example.split_even;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class Main_menu extends AppCompatActivity {
 
     Button mManageShoppingCartBtn;
     Button mLogOffBtn;
     Button mExitAppBtn;
+    Button approveNewItemBt;
 
 
     @Override
@@ -30,12 +23,20 @@ public class Main_menu extends AppCompatActivity {
         mManageShoppingCartBtn = findViewById(R.id.go_to_manage_cart_pageBt);
         mLogOffBtn = findViewById(R.id.log_of_mainBt);
         mExitAppBtn = findViewById(R.id.exit_app_mainBt);
+        approveNewItemBt = findViewById(R.id.approve_new_itemBt);
 
 
         mManageShoppingCartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 moveToManageShoppingCart();
+            }
+        });
+
+        approveNewItemBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToApproveNewItemBt();
             }
         });
 
@@ -57,6 +58,11 @@ public class Main_menu extends AppCompatActivity {
 
 
 }
+
+    private void moveToApproveNewItemBt() {
+        Intent intent = new Intent(getApplicationContext(), approveNewItem.class);
+        startActivity(intent);
+    }
 
     private void moveToOpeningScreen() {
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
